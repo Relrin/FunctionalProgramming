baa1afd7521d95037038c780ad79470111ffad8b
Valery Savich <Relrin78@gmail.com>
2014-11-17 02:25:13 +0300

    Add primitive (refs) for multithrething and correcting relative links

--- lab2/src/lab2/core.clj
+++ lab2/src/lab2/core.clj
@@ -5,6 +5,8 @@
   (:require [clojure.string    :as string])
   (:require [clj-http.client   :as client])  
   (:require [net.cgrand.enlive-html :as html])
+  (:refer-clojure :exclude [resolve])
+  (:use [clojurewerkz.urly.core])
   (:use [slingshot.slingshot])
 )
 
@@ -12,17 +14,38 @@
 ; TODO: add multithreading (using atoms/vars/agents/etc.)
 ; TODO: extract all HREFs from page (recursively)
 
+;-----------------------------------
+(def tree (ref []))
+
+
+(defn get-tree []
+  ; Getting vectors with urls
+  (deref tree))
+
+
+(defn add-url-from-file [val]
+  ; Add new URL at the end of vector
+  (dosync
+     (alter tree conj val)
+     (alter tree conj  []))) 
+
+
+(defn add-urls-on-index [item index]
+  ; Inserting vector with URLs after element with some index
+  (let [tmpTree (get-tree)]
+    (dosync 
+       (alter tree assoc index item))))
+;------------------------------------
 
 (defn read-urls [filepath]
   ; Reading URLs from *.txt file
   ; all URLs will be in one vector
   ; its seems like [link1 link2 ... linkN]
-  (let [data (atom [])]
   (with-open [rdr (io/reader filepath)]
     (doseq [line (line-seq rdr)]
        (if (not= line "")
-           (swap! data conj line))))
-  @data))
+           (add-url-from-file line))))
+  (get-tree))
 
 
 (defn delete-element [vc pos]
@@ -32,19 +55,36 @@
          (subvec vc (inc pos)))))
 
 
+(defn relativeURL? [itemURL]
+  ; checking URL on relativity
+  ; NOTE: symbol '#' means, that its not URL, its anchor 
+  (and (= (.indexOf itemURL "/") 0) (= (.indexOf itemURL "#") -1)))
+
+
+(defn absoluteURL? [itemURL]
+  ; checking URL on absoluteness
+  (or (= (.indexOf itemURL "http://") 0) (= (.indexOf itemURL "https://") 0)))
+
+
+(defn resolve-links [baseURL vector-links]
+  ; Resolve all links
+  ; See (example section): https://github.com/michaelklishin/urly
+  (let [relative-links (vec (filter relativeURL? vector-links))
+        absolute-links (vec (filter absoluteURL? vector-links))]
+    (into [] (distinct (concat (vec (map #(str (resolve (java.net.URI. baseURL) %)) relative-links)) absolute-links)))))
+
+
 (defn find-href [url-string]
-  ; Finding all HREFs in <body> tag
+  ; Finding all HREFs. Returns vector with corrected links
   (let [url (html/html-resource (java.net.URL. (str url-string)))]
-      (vec (map #(-> % :attrs :href) (html/select url [:a])))
-  )
-)
+    (resolve-links url-string (vec (keep #(-> % :attrs :href) (html/select url [:a]))))))
 
 
-(defn parse-url [url-string deep max-deep]
+(defn parse-url [url-string]
   ; TODO: add count in vector for result value
-  ; Processing URL
+  ; TODO: correct also with redirect version
   ; NOTE:
-  ; if get "error 404", then returns "bad"
+  ; if get "error 404/500/504", then returns "bad"
   ; if catch redirect, then return "redirect" + chain of links (but take last URL)
   (try+
     (let [downloaded-html (client/get url-string)]
@@ -54,9 +94,19 @@
         (str url-string " redirect " (get (vec (take-last 1 (get downloaded-html :trace-redirects))) 0))
       )
     ) 
-    ; exception handler for error 404 
+    ; exception handler for error 404/504
     (catch [:status 404] {:keys [trace-redirects]}
+      (str url-string " bad"))
+    (catch [:status 500] {:keys [trace-redirects]}
+      (str url-string " bad"))
+    (catch [:status 504] {:keys [trace-redirects]}
       (str url-string " bad"))))
+
+
+(defn thread-parser [url-string index max-deep]
+  ; TODO: Fill actions...
+  (add-urls-on-index (parse-url url-string) index)
+)
 
 
 (defn print-tree
@@ -70,12 +120,14 @@
       (str prefix tree "\n"))))
 
 
-(defn parse-to-tree [filepath deep]
+(defn parse-to-tree [filepath max-deep]
   ; Parsing urls from files to tree   
   (let [urls (read-urls filepath)  ; vector with URLs
         n-threads 25]              ; count of parallel threads (change me)
-  ;(println (print-tree test-tree))
-  (println (parse-url "http://twitter.com" 0 deep-lvl))
+  ; some logic for multithreading...
+  (thread-parser "http://dev.by/" 1 max-deep)
+  (thread-parser "http://habrahabr.ru/" 3 max-deep)
+  (println (print-tree (get-tree)))
 ))
 
 
