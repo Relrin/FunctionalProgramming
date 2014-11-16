71606eb379b2b1a144b7ce5f73638f11b1ecf91a
Valery Savich <Relrin78@gmail.com>
2014-11-17 02:24:23 +0300

    Add dependency for project

--- lab2/project.clj
+++ lab2/project.clj
@@ -1,5 +1,5 @@
 (defproject lab2 "0.1.0-SNAPSHOT"
-  :description "Lab #2: WebCrawler"
+  :description "Lab #2: Web crawler"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -7,7 +7,8 @@
                  [org.clojure/tools.cli "0.3.1"]
 		 [clj-http "1.0.1"]
                  [enlive "1.1.5"]
-                 [slingshot "0.12.1"]]
+                 [slingshot "0.12.1"]
+                 [clojurewerkz/urly "1.0.0"]]
   :main ^:skip-aot lab2.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
