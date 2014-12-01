8ec055e8a74217665b6a10110b7c0c09d6d08791
Valeryi Savich <Relrin78@gmail.com>
2014-12-02 00:02:13 +0300

    Add pmap, atom and tree structure for multithreading

--- lab2/src/lab2/core.clj
+++ lab2/src/lab2/core.clj
@@ -11,48 +11,58 @@
 )
 
 
-; TODO: add multithreading (using atoms/vars/agents/etc.)
-; TODO: extract all HREFs from page (recursively)
-
-;-----------------------------------
-(def tree (ref []))
+(defn uuid []
+  ; generate random UUID
+  (str (java.util.UUID/randomUUID)))
 
 
-(defn get-tree []
-  ; Getting vectors with urls
-  (deref tree))
+(defn create-node [id parent uris depth uri status children location]
+  ; create node in the tree
+  {:id id :parent parent :uris uris :depth depth :uri uri :status status :children children :location location})
 
 
-(defn add-url-from-file [val]
-  ; Add new URL at the end of vector
-  (dosync
-     (alter tree conj val)
-     (alter tree conj  []))) 
+(defn create-root-node [urls depth]
+  (create-node (uuid) nil urls depth "root" nil (atom []) nil))
+;------------------------------
+
+(defn read-file [filepath]
+  ; reading data from file
+  ; scheme: read all file as string -> split by '\n'
+  (clojure.string/split-lines (slurp filepath)))
 
 
-(defn add-urls-on-index [item index]
-  ; Inserting vector with URLs after element with some index
-  (let [tmpTree (get-tree)]
-    (dosync 
-       (alter tree assoc index item))))
-;------------------------------------
-
-(defn read-urls [filepath]
-  ; Reading URLs from *.txt file
-  ; all URLs will be in one vector
-  ; its seems like [link1 link2 ... linkN]
-  (with-open [rdr (io/reader filepath)]
-    (doseq [line (line-seq rdr)]
-       (if (not= line "")
-           (add-url-from-file line))))
-  (get-tree))
+(defn not-nil? [x]
+  (not (nil? x)))
 
 
-(defn delete-element [vc pos]
-  ; Delete element from vector by index
-  (vec (concat 
-         (subvec vc 0 pos) 
-         (subvec vc (inc pos)))))
+(defn remove-nils [col]
+  ; remove nil from collection
+  (filter not-nil? col))
+
+
+(defn get-sutable-lines [filepath]
+  (remove-nils (read-file filepath)))
+
+
+(defn get-indent [n]
+  ; repeat N time delimeter
+  (apply str (repeat n " ")))
+
+
+(defn get-status-message [node]
+  (if (= (:status node) 404)
+    " bad"
+    (let [message (str " " (count (:uris node)) " link(s)")]
+      (if (not-nil? (:location node))
+        (str message " redirects " (:location node)))
+        message)))
+
+
+(defn print-formatted-line [node level]
+  (let [indent (* 2 level)
+        uri    (:uri node)
+        status-message (get-status-message node)]
+    (println (str (get-indent indent) uri status-message))))
 
 
 (defn relativeURL? [itemURL]
@@ -67,68 +77,90 @@
 
 
 (defn resolve-links [baseURL vector-links]
-  ; Resolve all links
-  ; See (example section): https://github.com/michaelklishin/urly
+  ; resolve all links
+  ; see (example section): https://github.com/michaelklishin/urly
   (let [relative-links (vec (filter relativeURL? vector-links))
         absolute-links (vec (filter absoluteURL? vector-links))]
     (into [] (distinct (concat (vec (map #(str (resolve (java.net.URI. baseURL) %)) relative-links)) absolute-links)))))
 
 
-(defn find-href [url-string]
-  ; Finding all HREFs. Returns vector with corrected links
-  (let [url (html/html-resource (java.net.URL. (str url-string)))]
-    (resolve-links url-string (vec (keep #(-> % :attrs :href) (html/select url [:a]))))))
+(defn get-valid-urls [baseURL content]
+  ; finding all HREFs. Returns vector with corrected links
+  (let [data (->> (html/select content #{[:a]})
+                  (map #(:href (:attrs %1)))
+                  (remove-nils))]
+  (resolve-links baseURL data)))
 
 
-(defn parse-url [url-string]
-  ; TODO: add count in vector for result value
-  ; TODO: correct also with redirect version
-  ; NOTE:
-  ; if get "error 404/500/504", then returns "bad"
-  ; if catch redirect, then return "redirect" + chain of links (but take last URL)
+(defn get-parent-id [parent]
+  ; return parent's ID
+  (if (nil? parent)
+    nil
+    (:id parent)))
+
+
+(defn fetch-url [url]
+  ; try to download HTML
   (try+
-    (let [downloaded-html (client/get url-string)]
-      (if (empty? (delete-element (get downloaded-html :trace-redirects) 0))
-        (find-href url-string)
-        ; get redirects before downloading page
-        (str url-string " redirect " (get (vec (take-last 1 (get downloaded-html :trace-redirects))) 0))
-      )
-    ) 
-    ; exception handler for error 404/504
-    (catch [:status 404] {:keys [trace-redirects]}
-      (str url-string " bad"))
-    (catch [:status 500] {:keys [trace-redirects]}
-      (str url-string " bad"))
-    (catch [:status 504] {:keys [trace-redirects]}
-      (str url-string " bad"))))
+    (client/get url {:throw-exceptions false})
+    (catch Object _ {:status 404 :headers nil})))
 
 
-(defn thread-parser [url-string index max-deep]
-  ; TODO: Fill actions...
-  (add-urls-on-index (parse-url url-string) index)
-)
+(defn get-formatted-content [raw-content]
+  (let [content-type (:content-type (:headers raw-content))]
+    (if (and (not= 404 (:status raw-content)) (boolean (re-find #"text/html" content-type)))
+      (html/html-snippet (:body raw-content))
+      nil)))
 
 
-(defn print-tree
-  ; Printing vector as a tree
-  ([tree] 
-   (apply str (map #(print-tree % "") tree)))
-  ([tree prefix]
-    (if (vector? tree)
-      (->> (map #(print-tree % (str prefix "  ")) tree)
-           (apply str))
-      (str prefix tree "\n"))))
+(defn get-location-header [raw-content]
+  ; checking on redirects
+  (let [status (:status raw-content)]
+    (if (boolean (some #(= status %) '(300 301 302 303 307)))
+      (:location (:headers raw-content))
+      nil)))
 
 
-(defn parse-to-tree [filepath max-deep]
-  ; Parsing urls from files to tree   
-  (let [urls (read-urls filepath)  ; vector with URLs
-        n-threads 25]              ; count of parallel threads (change me)
-  ; some logic for multithreading...
-  (thread-parser "http://dev.by/" 1 max-deep)
-  (thread-parser "http://habrahabr.ru/" 3 max-deep)
-  (println (print-tree (get-tree)))
-))
+(defn parse-specific-page [parent url depth]
+  ; parsing some URL 
+  ; (println url)
+  (let [raw-content (fetch-url url)
+        status      (:status raw-content)
+        id          (uuid)
+        formatted-content (get-formatted-content raw-content)
+        child (if (not-nil? formatted-content)
+                (create-node id (get-parent-id parent) (get-valid-urls url formatted-content) depth url status (atom []) (get-location-header raw-content))
+                (create-node id (get-parent-id parent) '() depth url status (atom []) (get-location-header raw-content)))]
+    (swap! (:children parent) conj child)
+    child))
+
+
+(defn visit-node [node urls depth]
+  ; parallel processing (using parallel map) URLs while depth > 1
+  ; else return node
+  (let [new-depth (dec depth)]
+    (if (< depth 1)
+      node
+      (doseq [child (pmap #(parse-specific-page node %1 depth) urls)] (visit-node child (:uris child) new-depth)))))
+
+
+(defn do-walk [node level]
+  ; printing value in node and go inner
+  (print-formatted-line node level)
+  (doseq [child @(:children node)] (do-walk child (inc level))))
+
+
+(defn walk-tree [root]
+  ; walking over the tree from root
+  (do-walk root 0))
+
+
+(defn parse-to-tree [filepath depth]
+  ; parsing urls from files to tree   
+  (let [urls (get-sutable-lines filepath)
+         parent (create-root-node urls depth)]
+     (visit-node parent urls depth)
+     parent))
 
 
 (defn -main [& args]
@@ -147,5 +179,5 @@
       (System/exit 0))
     ; Or process args and start work
     (if (and (:file opts) (:deep opts))
-      (println (string/join "\n" (parse-to-tree (:file opts) (:deep opts))))
+      (walk-tree (parse-to-tree (:file opts) (Integer. (:deep opts))))
       (println "Please, type path for file!"))))
