4652861073c5b52109971816c224c4f1eddececa
Valeryi Savich <Relrin78@gmail.com>
2014-12-02 00:28:53 +0300

    Add two unit-tests

--- lab2/test/lab2/core_test.clj
+++ lab2/test/lab2/core_test.clj
@@ -1,7 +1,20 @@
 (ns lab2.core-test
   (:require [clojure.test :refer :all]
-            [lab2.core :refer :all]))
+            [lab2.core :refer :all]
+            [net.cgrand.enlive-html :as html]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn load-html [filepath]
+  (html/html-snippet (slurp filepath)))
+
+
+(deftest parse-html
+  (testing "Parse html page correctly."
+    (let [content (load-html "test/lab2/devby.html")
+          uris (get-valid-urls "http://dev.by" content)]
+      (is (= (count uris) 156)))))
+
+
+(deftest page-not-found
+  (testing "Error 404"
+    (let [server-respone (fetch-url "http://www.google.ru/somebadlink")]
+      (is (= (:status server-respone) 404)))))
